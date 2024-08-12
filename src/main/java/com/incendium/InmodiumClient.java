3fe3271ae17e30b35e8c242a0d3af5c7cd49bb3f
koolkid94 <alexander.yuchi@gmail.com>
2024-08-12 13:03:26 -0700

    added holy wrath and holy ammo type

--- src/main/java/com/incendium/InmodiumClient.java
+++ src/main/java/com/incendium/InmodiumClient.java
@@ -25,6 +25,23 @@ public class InmodiumClient implements ClientModInitializer {
         ModelPredicateProviderRegistry.register(ModItems.MULTIPLEX_CROSSBOW, new Identifier("charged"), (stack, world, entity, seed) -> {
             return entity != null && CrossbowItem.isCharged(stack) ? 1.0F : 0.0F;
         });
+
+
+
+        ModelPredicateProviderRegistry.register(ModItems.HOLY_WRATH, new Identifier("pull"), (stack, world, entity, seed) -> {
+            if (entity == null) {
+                return 0.0F;
+            } else {
+                return CrossbowItem.isCharged(stack) ? 0.0F : (float)(stack.getMaxUseTime() - entity.getItemUseTimeLeft()) / (float)CrossbowItem.getPullTime(stack);
+            }
+        });
+        ModelPredicateProviderRegistry.register(ModItems.HOLY_WRATH, new Identifier("pulling"), (stack, world, entity, seed) -> {
+            return entity != null && entity.isUsingItem() && entity.getActiveItem() == stack && !CrossbowItem.isCharged(stack) ? 1.0F : 0.0F;
+        });
+        ModelPredicateProviderRegistry.register(ModItems.HOLY_WRATH, new Identifier("charged"), (stack, world, entity, seed) -> {
+            return entity != null && CrossbowItem.isCharged(stack) ? 1.0F : 0.0F;
+        });
+
     }
 
     @Override
--- src/main/java/com/incendium/item/ModItems.java
+++ src/main/java/com/incendium/item/ModItems.java
@@ -1,4 +1,5 @@
 package com.incendium.item;
+import com.incendium.item.custom.HolyWrathCrossbowItem;
 import net.minecraft.item.*;
 import net.minecraft.util.registry.Registry;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -9,10 +10,13 @@ import com.incendium.item.custom.MultiplexCrossbowItem;
 
 public class ModItems {
     public static final Item MULTIPLEX_CROSSBOW = regsiterItem("multiplex",
-            new MultiplexCrossbowItem(new FabricItemSettings().group(ItemGroup.COMBAT)));
+            new MultiplexCrossbowItem(new FabricItemSettings().maxDamage(198).group(ItemGroup.COMBAT)));
 
     public static final Item FISTFULL_OF_ARROWS = regsiterItem("fistfull_of_arrows",
             new Item (new FabricItemSettings().group(ItemGroup.COMBAT)));
+
+    public static final Item HOLY_WRATH = regsiterItem("holy_wrath",
+            new HolyWrathCrossbowItem(new FabricItemSettings().maxDamage(323).group(ItemGroup.COMBAT)));
 
 
     private static Item regsiterItem(String name, Item item) {
