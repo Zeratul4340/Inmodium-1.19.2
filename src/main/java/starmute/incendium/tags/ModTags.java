fb955fa2ef9a028228f678bdba93ff0b61fe33da
Zeratul4340 <eduardoatroom37@gmail.com>
2024-08-11 22:33:37 -0700

    Changes by alexa

---
+++ src/main/java/starmute/incendium/tags/ModTags.java
@@ -0,0 +1,11 @@
+package starmute.incendium.tags;
+
+import net.minecraft.item.Item;
+import net.minecraft.tag.TagKey;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public final class ModTags {
+
+}
