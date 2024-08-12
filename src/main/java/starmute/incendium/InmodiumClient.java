52551d385efb373d9b0197229adb9179316cb3ae
koolkid94 <alexander.yuchi@gmail.com>
2024-08-12 02:06:30 -0700

    working on predicates for charging animation

---
+++ src/main/java/starmute/incendium/InmodiumClient.java
@@ -0,0 +1,34 @@
+package starmute.incendium;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import starmute.incendium.item.ModItems;
+import starmute.incendium.item.custom.MultiplexCrossbowItem;
+
+public class InmodiumClient implements ClientModInitializer {
+    public static void registerModelPredicateProviders() {
+
+        ModelPredicateProviderRegistry.register(ModItems.MULTIPLEX_CROSSBOW, new Identifier("pull"), (stack, world, entity, seed) -> {
+            if (entity == null) {
+                return 0.0F;
+            } else {
+                return CrossbowItem.isCharged(stack) ? 0.0F : (float)(stack.getMaxUseTime() - entity.getItemUseTimeLeft()) / (float)CrossbowItem.getPullTime(stack);
+            }
+        });
+        ModelPredicateProviderRegistry.register(ModItems.MULTIPLEX_CROSSBOW, new Identifier("pulling"), (stack, world, entity, seed) -> {
+            return entity != null && entity.isUsingItem() && entity.getActiveItem() == stack && !CrossbowItem.isCharged(stack) ? 1.0F : 0.0F;
+        });
+        ModelPredicateProviderRegistry.register(ModItems.MULTIPLEX_CROSSBOW, new Identifier("charged"), (stack, world, entity, seed) -> {
+            return entity != null && CrossbowItem.isCharged(stack) ? 1.0F : 0.0F;
+        });
+    }
+
+    @Override
+    public void onInitializeClient() {
+        // ...
+        registerModelPredicateProviders();
+    }
+}
